c3773c763f715f102cebf76e5a6ac82fc0bc8da7
Logan Connor <loganconnor44@gmail>
2020-12-04 18:21:42 -0500

    stable version with nice loading bar

--- convert-m3u8-to-mkv.py
+++ convert-m3u8-to-mkv.py
@@ -1,17 +1,16 @@
 import argparse, sys
-from pathlib import Path
 import ffmpeg_streaming
 from yaspin import yaspin
 from yaspin.spinners import Spinners
-from codecs import decode
+import pathlib
 
 """
 @return: None
 """
-def convertStreamToMkv(httpPathToM3U8, outputName) :
+def convertStreamToMkv(httpPathToM3U8, outputNameAndPath) :
 	video = ffmpeg_streaming.input(httpPathToM3U8)
 	stream = video.stream2file(ffmpeg_streaming.Formats.h264())
-	stream.output(outputName)
+	stream.output(outputNameAndPath)
 
 """
 Core logic for the terminal application
@@ -19,10 +18,12 @@ Core logic for the terminal application
 """
 def mainApplication() :
 	parser=argparse.ArgumentParser()
-	parser.add_argument('--input', help='Specifies the input url file stream location that will be targeted.')
-	parser.add_argument('--output', help='Specifies the name of a output file from the input source.')
+	parser.add_argument('--input', help='Specifies the input url file stream location that will be targeted.', required=True)
+	parser.add_argument('--output', help='Specifies the name of a output file from the input source.', required=True)
 	parser.add_argument('--path', help='Specifies the where the input file resides and the output file will be created.')
 	args = parser.parse_args()
+
+	print('\n')
 
 	if args.input == None :
 		print("Please provide a URL location for the file you are attempting to convert.")
@@ -37,12 +38,22 @@ def mainApplication() :
 		output = args.output
 		print("Output file name and extension: " + output)
 
-	with yaspin(Spinners.arc, text="Downloading Video") as spnr :
-		spnr.write("Beginning Process")
-		convertStreamToMkv(inputPath, output)
-		spnr.write("Process Completed")
-		spnr.ok("Enjoy!")
+	if args.path == None : 
+		path = pathlib.Path.home() / 'Downloads'
+		print("Path not specified. Defaulting to macOS downloads.")
+	else : 
+		path = args.path
+
+	fullOutputPathWithFileNameAndExtension = path / output
+	print("Creating video at location: " + fullOutputPathWithFileNameAndExtension.as_posix())
 	
+	print('\n')
+
+	with yaspin(Spinners.arc, text="Downloading Video", color="blue") as spnr :
+		convertStreamToMkv(inputPath, fullOutputPathWithFileNameAndExtension.as_posix())
+		spnr.text = "Video Downloaded"
+		spnr.ok("Enjoy!")
+
 	return
 
 if __name__ == "__main__" :
